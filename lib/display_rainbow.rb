53adb5b1d5fd00f5355d497fd7bc250f3bd97a68
Daksha Patel <dakshapatel100@gmail.com>
2018-04-25 16:00:51 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,11 @@
 colors = ['R:red', 'O:orange', 'Y:yellow', 'G:green', 'B:blue', 'I:indigo', 'V:violet']
 
 def display_rainbow (colors)
-  puts "first color is : #{R:red [0]}"
-  puts "first color is : #{O:orange [1]}"
-  puts "first color is : #{Y:yellow [2]}"
+  puts "first color is : #{[0]}"
+  puts "first color is : #{[1]}"
+  puts "first color is : #{2]}"
+  puts "first color is : #{[3]}"
+  puts "first color is : #{[4]}"
+  puts "first color is : #{[5]}"
+  puts "first color is : #{[6]}"
 end
